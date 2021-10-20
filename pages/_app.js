0546a197665225cba1da5808949b15d43b0c04cf
Henry Pulver <henrypulver@hotmail.co.uk>
2021-10-20 14:02:56 +0100

    Update _app.js and add global css

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,24 @@
-import "tailwindcss/tailwind.css";
 import React from "react";
 import PropTypes from "prop-types";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import "tailwindcss/tailwind.css";
 import "../styles/global.css";
 import "../styles/tooltip.css";
+import "../styles/intro.css";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+import "tippy.js/themes/light.css";
+import "react-select2-wrapper/css/select2.css";
 
-export default function MyApp({ Component, pageProps }) {
-  console.log(Component);
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.node,
+App.propTypes = {
+  Component: PropTypes.func,
   pageProps: PropTypes.object,
 };
