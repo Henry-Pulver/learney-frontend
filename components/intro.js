c459f71587b81e09f7c9384f3300af493f55d42b
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-08 22:13:55 +0000

    Move icons to new file & fix intro slides not found

--- components/intro.js
+++ components/intro.js
@@ -2,6 +2,7 @@ import React from "react";
 import Tippy from "@tippyjs/react";
 import buttonStyles from "../styles/buttons.module.css";
 import { classNames } from "../lib/reactUtils";
+import { PrevArrow, NextArrow } from "./icons";
 
 export default function IntroButtonInclTooltip({
   introShown,
@@ -81,7 +82,11 @@ export function IntroSection({
         <div className={"intro-gif-div"}>
           <video
             className={"intro-gif"}
-            src={introSlides ? introSlides[introSlideNumber].gif : ""}
+            src={
+              introSlides
+                ? `${location.protocol}//${location.host}/${introSlides[introSlideNumber].gif}`
+                : ""
+            }
             autoPlay
             loop
             muted
@@ -144,18 +149,7 @@ function SlideDiv({ prevSlide, nextSlide, introSlideNumber, numSlides }) {
           "text-white border border-transparent text-sm font-medium rounded-lg inline-flex items-center px-4 py-2"
         )}
       >
-        <svg
-          className="w-5 h-5 mr-2"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <PrevArrow />
         Prev
       </button>
       <div className={"text-gray-700 px-4 py-2"}>
@@ -171,18 +165,7 @@ function SlideDiv({ prevSlide, nextSlide, introSlideNumber, numSlides }) {
         )}
       >
         Next
-        <svg
-          className="w-5 h-5 ml-2"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <NextArrow />
       </button>
     </div>
   );
