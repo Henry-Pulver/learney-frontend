e224e41fda1a9bbc3ff6b223bf61ba6f862295e9
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-09 10:48:47 +0000

    Make link previews show if visited

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['visited'],
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
