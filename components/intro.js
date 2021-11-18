1819ae08f85ca02fb84c3c99750a1ab8571327a7
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-18 11:22:08 +0000

    Change intro to use modal and remove intro styles

--- components/intro.js
+++ components/intro.js
@@ -1,15 +1,69 @@
-import React from "react";
-import Tippy from "@tippyjs/react";
-import buttonStyles from "../styles/buttons.module.css";
-import { classNames } from "../lib/reactUtils";
+import React, { useState, useRef } from "react";
+import { InformationCircleIcon } from "@heroicons/react/outline";
+import { IconButtonTippy } from "./buttons";
+import Modal from "./modal";
 
-export default function IntroButtonInclTooltip({
-  introShown,
-  hideIntroTooltip,
-  showIntroTooltip,
-  buttonPressFunction,
-}) {
+export default function IntroButton({ openAtStart, buttonPressFunction }) {
+  const [introShown, setIntroShown] = useState(openAtStart);
+
+  // Here so the slide number is remembered between closing & opening the modal
   const [introSlideNumber, setIntroSlide] = React.useState(0);
+
+  const [introSlidesJson, setIntroSlidesJson] = React.useState(null);
+  // TODO: replace below with useAsync() so it's in line with the rest of the codebase
+  React.useEffect(() => {
+    (async function () {
+      const response = await fetch("/introSlides.json");
+      const slides = await response.json();
+      setIntroSlidesJson(slides);
+    })();
+  }, []);
+
+  // So the 'next slide' button is focused on load
+  const initialFocus = useRef(null);
+
+  return (
+    <>
+      <IconButtonTippy content="How to use Learney">
+        <button
+          className="mobile-icon-button lg:gray-icon-btn"
+          onClick={
+            introShown
+              ? () => setIntroShown(false)
+              : () => setTimeout(() => setIntroShown(true), 1000)
+          }
+        >
+          <div className="block lg:hidden px-2 sm:px-4 text-black">
+            How to use Learney
+          </div>
+          <span className="sr-only">Information</span>
+          <InformationCircleIcon className="h-7 w-7" />
+        </button>
+      </IconButtonTippy>
+      <Modal
+        open={introShown}
+        setOpen={setIntroShown}
+        initialFocus={initialFocus}
+      >
+        <IntroSection
+          introSlideNumber={introSlideNumber}
+          setIntroSlide={setIntroSlide}
+          introSlides={introSlidesJson}
+          buttonPressFunction={buttonPressFunction}
+          initialFocus={initialFocus}
+        />
+      </Modal>
+    </>
+  );
+}
+
+export function IntroSection({
+  introSlideNumber,
+  setIntroSlide,
+  introSlides,
+  buttonPressFunction,
+  initialFocus,
+}) {
   const prevSlide = buttonPressFunction(
     () => setIntroSlide(introSlideNumber - 1),
     "prev-intro"
@@ -19,85 +73,35 @@ export default function IntroButtonInclTooltip({
     "next-intro"
   );
 
-  const closeIntroButton = buttonPressFunction(hideIntroTooltip, "close-intro");
-
-  const [introSlidesJson, setIntroSlidesJson] = React.useState(null);
-  React.useEffect(() => {
-    (async function () {
-      const response = await fetch("/introSlides.json");
-      const slides = await response.json();
-      setIntroSlidesJson(slides);
-    })();
-  }, []);
-
   return (
-    <Tippy
-      visible={introShown}
-      theme={"light"}
-      onClickOutside={hideIntroTooltip}
-      arrow={true}
-      placement={"bottom"}
-      hideOnClick={false}
-      interactive={true}
-      maxWidth={"auto"}
-      content={
-        <IntroSection
-          introSlides={introSlidesJson}
-          prevSlide={prevSlide}
-          nextSlide={nextSlide}
-          introSlideNumber={introSlideNumber}
-          closeIntro={closeIntroButton}
-        />
-      }
-    >
-      <button
-        className={`${buttonStyles.button} ${buttonStyles.large}`}
-        id="open-intro"
-        onClick={introShown ? hideIntroTooltip : showIntroTooltip}
-      >
-        Intro
-      </button>
-    </Tippy>
-  );
-}
-
-export function IntroSection({
-  introSlides,
-  prevSlide,
-  nextSlide,
-  introSlideNumber,
-  closeIntro,
-}) {
-  return (
-    <div className={"intro-content-container"}>
-      <button className={`close ${buttonStyles.button}`} onClick={closeIntro}>
-        {"X"}
-      </button>
-      <div className={"intro-container"}>
-        <h2 className={"intro-text"}>
-          {introSlides ? introSlides[introSlideNumber].title : ""}
-        </h2>
-        <div className={"intro-gif-div"}>
-          <video
-            className={"intro-gif"}
-            src={introSlides ? introSlides[introSlideNumber].gif : ""}
-            autoPlay
-            loop
-            muted
-          />
-        </div>
-        {/*<IntroTextSection slideInfo={introSlides ? introSlides[introSlideNumber] : null}/>*/}
-        <IntroTextSection
-          introSlides={introSlides}
-          introSlideNumber={introSlideNumber}
-        />
-        <SlideDiv
-          prevSlide={prevSlide}
-          nextSlide={nextSlide}
-          introSlideNumber={introSlideNumber}
-          numSlides={introSlides ? introSlides.length : 0}
+    <div className="text-black block">
+      <h2 className="text-black m-2 text-2xl font-bold">
+        {introSlides ? introSlides[introSlideNumber].title : ""}
+      </h2>
+      <div className="flex mb-4">
+        <video
+          className="m-auto"
+          src={
+            introSlides
+              ? `${location.protocol}//${location.host}/${introSlides[introSlideNumber].gif}`
+              : ""
+          }
+          autoPlay
+          loop
+          muted
         />
       </div>
+      <IntroTextSection
+        introSlides={introSlides}
+        introSlideNumber={introSlideNumber}
+      />
+      <SlideDiv
+        prevSlide={prevSlide}
+        nextSlide={nextSlide}
+        introSlideNumber={introSlideNumber}
+        numSlides={introSlides ? introSlides.length : 0}
+        initialFocus={initialFocus}
+      />
     </div>
   );
 }
@@ -113,35 +117,48 @@ function IntroTextSection({ introSlides, introSlideNumber }) {
       let textItem = slideInfo.text[i];
       if (typeof textItem === "string") {
         if (textItem === "br") {
-          elementArray.push(<br />);
-          elementArray.push(<br />);
+          elementArray.push(<br key={elementArray.length} />);
+          elementArray.push(<br key={elementArray.length} />);
         } else {
           elementArray.push(textItem);
         }
       } else if (textItem[0] === "a") {
-        elementArray.push(<a {...textItem[1]}>{textItem[2]}</a>);
+        elementArray.push(
+          <a
+            className="underline text-blue-500 hover:text-blue-600 cursor-pointer visited:text-pink-500"
+            {...textItem[1]}
+            key={elementArray.length}
+          >
+            {textItem[2]}
+          </a>
+        );
       } else if (textItem[0] === "b") {
-        elementArray.push(<b {...textItem[1]}>{textItem[2]}</b>);
+        elementArray.push(
+          <b {...textItem[1]} key={elementArray.length}>
+            {textItem[2]}
+          </b>
+        );
       }
     }
   }
-  return <div className={"intro-text-div"}>{elementArray}</div>;
+  return <div className="py-4">{elementArray}</div>;
 }
 
-function SlideDiv({ prevSlide, nextSlide, introSlideNumber, numSlides }) {
+function SlideDiv({
+  prevSlide,
+  nextSlide,
+  introSlideNumber,
+  numSlides,
+  initialFocus,
+}) {
   let firstSlide = introSlideNumber === 0;
   let lastSlide = introSlideNumber === numSlides - 1;
 
   return (
-    <div className="intro-slide-change-div">
+    <div className="pt-4 pb-2 m-auto inline-flex">
       <button
         onClick={firstSlide ? () => {} : prevSlide}
-        className={classNames(
-          firstSlide
-            ? "bg-gray-300 cursor-default "
-            : "cursor-pointer bg-blue-600 hover:bg-blue-500 ",
-          "text-white border border-transparent text-sm font-medium rounded-lg inline-flex items-center px-4 py-2"
-        )}
+        className={firstSlide ? "btn-deactivated" : "btn-blue"}
       >
         <svg
           className="w-5 h-5 mr-2"
@@ -157,17 +174,13 @@ function SlideDiv({ prevSlide, nextSlide, introSlideNumber, numSlides }) {
         </svg>
         Prev
       </button>
-      <div className={"text-gray-700 px-4 py-2"}>
+      <div className="text-center text-gray-500 px-4 py-2">
         {(introSlideNumber + 1).toString() + "/" + numSlides.toString()}
       </div>
       <button
         onClick={lastSlide ? () => {} : nextSlide}
-        className={classNames(
-          lastSlide
-            ? "bg-gray-300 cursor-default "
-            : "cursor-pointer bg-blue-600 hover:bg-blue-500 ",
-          "text-white border border-transparent text-sm font-medium rounded-lg inline-flex items-center px-4 py-2"
-        )}
+        ref={initialFocus}
+        className={lastSlide ? "btn-deactivated" : "btn-blue"}
       >
         Next
         <svg
