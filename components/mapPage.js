785bd7f5df174d909d6ee3b18f9decbe272fd2a7
Henry Pulver <henrypulver@hotmail.co.uk>
2021-11-29 12:06:48 +0000

    Move editor logic to Editor component
    When Steve initially wrote the map editor, he chucked all
     the code into MapPage.js (overall component containing
    everything), so this commit tidies this up.

--- components/mapPage.js
+++ components/mapPage.js
@@ -1,17 +1,12 @@
 import ReactGA from "react-ga";
 import {
-  AddEdgesButton,
-  AddNodeButton,
-  CursorButton,
-  DeleteElementButton,
   FeedBackButton,
   MakeSuggestionIconButton,
-  ResetPanButton,
-  ResetProgressIconButton,
-  saveMap,
   SaveMapButton,
   SlackButton,
   MapSettingsIconButton,
+  ResetPanButton,
+  ResetProgressIconButton,
 } from "./buttons";
 import React, { useEffect } from "react";
 import {
@@ -35,32 +30,8 @@ import {
   initialiseSignInTooltip,
 } from "../lib/learningAndPlanning";
 import Navbar from "./navbar";
-import {
-  EditConceptDataSidebar,
-  EditTopicDataSidebar,
-} from "./editor/editConceptDataSidebar";
-import { AreYouSureModal, getAreYouSureDescriptionText } from "./modal";
+import Editor from "./editor/editor";
 // import SearchBar, {getSearchOptions} from "./search";
-
-let eh; // Edge handles variable
-const topicColours = [
-  "#001d4d",
-  "#006161",
-  "#001975",
-  "#001d4d",
-  "#210042",
-  "#610061",
-];
-const emptyNodeData = {
-  id: Infinity,
-  name: "",
-  lectures: "",
-  description: "",
-  urls: "",
-  nodetype: "concept",
-  relative_importance: 1,
-  parent: "",
-};
 
 export default function MapPage({
   backendUrl,
@@ -155,214 +126,6 @@ export default function MapPage({
     })();
   }, [isLoading]);
 
-  const [editType, setEditType] = React.useState("cursor");
-  const addNode = function (e) {
-    // [1.0] Create the next node ID
-    let nextNodeID = 0;
-    window.cy
-      .nodes('[nodetype = "concept"]') // Get concept nodes only
-      .forEach((node) => {
-        // Find the largest concept ID number
-        if (Number(node.data().id) > nextNodeID)
-          nextNodeID = Number(node.data().id);
-      });
-    // The next node ID is the largest previous node ID number + 1
-    nextNodeID += 1;
-    let nodeClicked = e.target;
-
-    // [2.0] Get parent node if one was clicked!
-    let parentNodeId;
-    let newParentNodeData;
-    if ("nodetype" in nodeClicked.data()) {
-      // Clicked on a node or parent node!
-      if (nodeClicked.data().nodetype === "field") {
-        parentNodeId = nodeClicked.data().id;
-      } else {
-        parentNodeId = nodeClicked.parent().data().id;
-      }
-    } else {
-      // Clicked in empty space - create new parent node!
-      parentNodeId = `${nextNodeID}_topic_group`;
-      newParentNodeData = {
-        group: "nodes",
-        data: {
-          colour: topicColours[Math.floor(Math.random() * topicColours.length)],
-          id: parentNodeId,
-          name: "",
-          nodetype: "field",
-        },
-        renderedPosition: {
-          x: e.renderedPosition.x,
-          y: e.renderedPosition.y,
-        },
-      };
-    }
-
-    // [2.1] Create new node
-    const newNode = {
-      data: {
-        id: `${nextNodeID}`,
-        name: "",
-        lectures: "",
-        description: "",
-        urls: [],
-        nodetype: "concept",
-        relative_importance: 1,
-        parent: parentNodeId,
-      },
-      renderedPosition: {
-        x: e.renderedPosition.x,
-        y: e.renderedPosition.y,
-      },
-    };
-
-    // [3.0] Add the new nodes
-    if (newParentNodeData !== undefined)
-      window.cy.add([newParentNodeData, newNode]);
-    else window.cy.add([newNode]);
-
-    // [4.0] Update UI
-    setEditNodeData(newNode.data);
-    setEditType("cursor");
-    setShowEditData("concept");
-  };
-  const deleteModeClick = function (e) {
-    if (e.target.data().id !== undefined) {
-      setDeleteNodeData((prevState) => {
-        return { ...prevState, ...e.target.data() };
-      });
-    }
-  };
-  const removeElement = function (elementId) {
-    const element = window.cy.nodes(`[id = "${elementId}"]`);
-    const parentNode = element.parent();
-    element.remove();
-    if (parentNode.isChildless()) parentNode.remove();
-  };
-  const handleEditNodeData = function (e) {
-    setEditNodeData(e.target.data());
-    setShowEditData("concept");
-  };
-
-  useEffect(() => {
-    // [1.0] Ensure cytoscape map is initialized
-    if (window.cy.edgehandles) {
-      // [2.0] Remove old event listeners
-      window.cy.removeListener("tap");
-      if (eh !== undefined) eh.disableDrawMode();
-
-      // [3.0] Add new event listeners
-      switch (editType) {
-        case "addNode":
-          window.cy.on("tap", addNode);
-          break;
-        case "addEdges":
-          eh = window.cy.edgehandles({
-            canConnect: function (sourceNode, targetNode) {
-              // whether an edge can be created between source and target
-              return (
-                !sourceNode.same(targetNode) && // No loops
-                !sourceNode.isParent() && // No links from parent nodes
-                !targetNode.isParent() // No links to parent nodes
-              );
-            },
-            noEdgeEventsInDraw: false,
-            snap: false,
-          });
-
-          eh.enableDrawMode();
-          break;
-        case "cursor":
-          window.cy.on("tap", 'node[nodetype = "concept"]', handleEditNodeData);
-          window.cy.on(
-            "tap",
-            'node[nodetype = "field"]',
-            handleEditParentNodeData
-          );
-          break;
-        case "delete":
-          window.cy.on("tap", deleteModeClick);
-          break;
-        default:
-          break;
-      }
-    }
-  }, [editType]);
-
-  const [showEditData, setShowEditData] = React.useState(null);
-  const [deleteNodeData, setDeleteNodeData] = React.useState({
-    ...emptyNodeData,
-  });
-  const [editNodeData, setEditNodeData] = React.useState({ ...emptyNodeData });
-  const saveEditNodeData = function (userId) {
-    // [1.0] Copy Previous Data
-    let newNodeData = { ...editNodeData };
-
-    // [2.0] Convert url string to array
-    if (typeof newNodeData.urls === "string")
-      newNodeData.urls = newNodeData.urls.split(",");
-
-    // [3.0] If parent was changed, move node to new parent
-    const oldParent = window.cy.getElementById(newNodeData.id).data("parent");
-    if (oldParent !== newNodeData.parent) {
-      // [3.1] If the new parent doesn't exist, create new parent node
-      if (
-        window.cy.getElementById(newNodeData.parent.replace(/ /g, "_")) !==
-        undefined
-      ) {
-        const newParentNode = {
-          group: "nodes",
-          data: {
-            colour:
-              topicColours[Math.floor(Math.random() * topicColours.length)],
-            id: newNodeData.parent.replace(/ /g, "_"),
-            name: newNodeData.parent,
-            nodetype: "field",
-          },
-          renderedPosition: {
-            x: window.cy.getElementById(newNodeData.id).x,
-            y: window.cy.getElementById(newNodeData.id).y,
-          },
-        };
-
-        window.cy.add([newParentNode]);
-      }
-
-      // [3.2] Move node to new parent
-      window.cy
-        .getElementById(newNodeData.id)
-        .move({ parent: newNodeData.parent.replace(/ /g, "_") });
-
-      // [3.3] Remove empty parent nodes
-      window.cy.nodes('[nodetype = "field"]').forEach((node) => {
-        if (node.isChildless()) node.remove();
-      });
-    }
-
-    // [4.0] Update node data
-    window.cy.getElementById(newNodeData.id).data(newNodeData);
-
-    // [5.0] Save the results
-    saveMap(userId, backendUrl, mapUUID);
-    setShowEditData(null);
-  };
-
-  const [editParentNodeData, setEditParentNodeData] = React.useState({
-    colour: "",
-    id: "",
-    name: "",
-    nodetype: "",
-  });
-  const handleEditParentNodeData = function (e) {
-    setEditParentNodeData(e.target.data());
-    setShowEditData("topic");
-  };
-  const saveEditParentNodeData = function () {
-    const newParentNodeData = { ...editParentNodeData };
-    window.cy.getElementById(newParentNodeData.id).data(newParentNodeData);
-    setShowEditData(null);
-  };
-
   return (
     <div>
       <MapHeader editMode={editMap} mapUrlExtension={mapUrlExtension} />
@@ -442,37 +205,7 @@ export default function MapPage({
         goals={goals}
         onSetGoalClick={onSetGoalClick}
         setGoalsState={setGoalsState}
-        handleEditNodeData={handleEditNodeData}
-        handleEditParentNodeData={handleEditParentNodeData}
       />
-
-      {editMap && (
-        <div
-          className={`flex flex-row items-end absolute bottom-0 left-0 m-4 disableTouchActions`}
-        >
-          <div className="block bg-white cursor-pointer rounded-lg m-1 z-20">
-            <CursorButton editType={editType} setEditType={setEditType} />
-            <AddNodeButton editType={editType} setEditType={setEditType} />
-            <AddEdgesButton editType={editType} setEditType={setEditType} />
-            <DeleteElementButton
-              editType={editType}
-              setEditType={setEditType}
-            />
-            <AreYouSureModal
-              modalShown={!!deleteNodeData.name}
-              setModalClosed={() => setDeleteNodeData(() => emptyNodeData)}
-              titleText={`Delete ${deleteNodeData.nodetype} ${deleteNodeData.name}?`}
-              descriptionText={
-                deleteNodeData.name
-                  ? getAreYouSureDescriptionText(deleteNodeData)
-                  : ""
-              }
-              actionButtonText={`Delete ${deleteNodeData.nodetype}`}
-              actionButtonFunction={() => removeElement(deleteNodeData.id)}
-            />
-          </div>
-        </div>
-      )}
       <div
         className={`flex flex-row items-end absolute bottom-0 right-0 mx-8 my-4 disableTouchActions`}
       >
@@ -489,55 +222,16 @@ export default function MapPage({
           />
         )}
       </div>
-
       {editMap && (
-        <div
-          className={
-            "cursor-default pointer-events-none absolute bottom-5 text-center w-full text-lg text-white z-10"
-          }
-        >
-          {editType === "cursor"
-            ? "Click concepts or topics to edit and drag to move them"
-            : editType === "addEdges"
-            ? "Click and hold to add a dependency"
-            : editType === "addNode"
-            ? "Click to add a concept"
-            : "Delete a concept, subject or dependency by clicking on it"}
-        </div>
-      )}
-      {showEditData === "concept" ? (
-        <EditConceptDataSidebar
-          editNodeData={editNodeData}
-          setEditNodeData={setEditNodeData}
-          setShowEditData={setShowEditData}
-          saveEditNodeData={saveEditNodeData}
-          deletebuttonClickFunction={(nodeId) =>
-            setDeleteNodeData((prevState) => {
-              return {
-                ...prevState,
-                ...window.cy.nodes(`[id = "${nodeId}"]`).data(),
-              };
-            })
-          }
+        <Editor
+          buttonPressFunction={buttonPressFunction}
+          backendUrl={backendUrl}
           userId={userId}
+          mapUUID={mapUUID}
+          sessionId={sessionId}
+          setGoalsState={setGoalsState}
+          setLearnedState={setLearnedState}
         />
-      ) : (
-        showEditData === "topic" && (
-          <EditTopicDataSidebar
-            editParentNodeData={editParentNodeData}
-            setEditParentNodeData={setEditParentNodeData}
-            saveEditParentNodeData={saveEditParentNodeData}
-            deletebuttonClickFunction={(nodeId) =>
-              setDeleteNodeData((prevState) => {
-                return {
-                  ...prevState,
-                  ...window.cy.nodes(`[id = "${nodeId}"]`).data(),
-                };
-              })
-            }
-            setShowEditData={setShowEditData}
-          />
-        )
       )}
     </div>
   );
