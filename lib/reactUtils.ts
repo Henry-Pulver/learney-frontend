5d7b3c68c3a4d01637a0415ecf51c271500feae3
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-02 19:01:38 +0000

    Add appendToArray function

--- lib/reactUtils.ts
+++ lib/reactUtils.ts
@@ -1,3 +1,9 @@
 export function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
+
+export function appendToArray(array: Array<any>, extraElement: any): Array<any> {
+  if (extraElement)
+    array.push(extraElement);
+  return array;
+}
