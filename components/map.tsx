f120358b85a5af6a386629edf7df290225b9b3e3
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-03 11:28:08 +0000

    Make map shrink when conceptInfo open

--- components/map.tsx
+++ components/map.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
-import { ConceptTippy } from "./conceptInfo";
+import { ConceptInfo } from "./conceptInfo";
 import { getDataFromStorage, saveVote } from "../lib/tooltips";
 import {
   setGoalNodesGlobal,
@@ -14,6 +14,20 @@ import { setupCtoCentre } from "../lib/hotkeys";
 import { classNames } from "../lib/reactUtils";
 import { ButtonPressFunction } from "../lib/types";
 import { EditType } from "./editor/types";
+import { useAsync } from "react-async";
+import { fetchTotalVotes } from "../lib/utils";
+import {
+  ElementsDefinition,
+  NodeSingular,
+  SingularElementArgument,
+  Stylesheet,
+} from "cytoscape";
+import {
+  GetNextConceptButton,
+  ResetPanButton,
+  ResetProgressIconButton,
+} from "./buttons";
+import { getNextNodeToLearn } from "../lib/questions";
 
 export default function Map({
   backendUrl,
@@ -31,6 +45,7 @@ export default function Map({
   goals,
   onSetGoalClick,
   setGoalsState,
+  pageLoaded,
   setPageLoaded,
   editType,
 }: {
@@ -39,7 +54,7 @@ export default function Map({
   userEmail: string;
   allowSuggestions: boolean;
   editMap: boolean;
-  mapJson: object;
+  mapJson: ElementsDefinition;
   mapUUID: string;
   sessionId: string;
   buttonPressFunction: ButtonPressFunction;
@@ -47,8 +62,13 @@ export default function Map({
   onLearnedClick: Function;
   setLearnedState: Function;
   goals: object;
-  onSetGoalClick: Function;
+  onSetGoalClick: (
+    node: NodeSingular,
+    userId: string,
+    sessionId: string
+  ) => void;
   setGoalsState: Function;
+  pageLoaded: boolean;
   setPageLoaded: (boolean) => void;
   editType: EditType;
 }) {
@@ -65,17 +85,13 @@ export default function Map({
   };
 
   const cytoscapeRef = useRef();
-  const [nodeSelected, setNodeSelected] = React.useState(null);
-  const [conceptTippyShown, setConceptTippyShown] = React.useState(false);
-  const [hoverNode, setHoverNode] = React.useState(false);
+  const [nodeSelected, setNodeSelected] = React.useState<
+    NodeSingular | undefined
+  >(undefined);
+  const showConceptPanel: (NodeSingular) => void = (node) => setNodeSelected(node);
+  const hideConceptPanel: () => void = () => setNodeSelected(undefined);
 
-  const showConceptTippy = function (node) {
-    setNodeSelected(node);
-    setConceptTippyShown(true);
-  };
-  const hideConceptTippy = function () {
-    setConceptTippyShown(false);
-  };
+  const [hoverNode, setHoverNode] = React.useState<boolean>(false);
 
   useEffect(() => {
     (async function () {
@@ -106,8 +122,8 @@ export default function Map({
           userId,
           mapUUID,
           sessionId,
-          showConceptTippy,
-          hideConceptTippy,
+          showConceptPanel,
+          hideConceptPanel,
           onSetGoalClick,
           editMap,
           router,
@@ -119,40 +135,83 @@ export default function Map({
       }
     })();
   }, [sessionId, userId]);
+  const [nextConcept, setNextConcept] = useState<SingularElementArgument>(null);
+
+  useEffect(() => {
+    if (!editMap && pageLoaded) setNextConcept(getNextNodeToLearn());
+  }, [pageLoaded, learned, goals]);
+
+  const { data } = useAsync({
+    promiseFn: fetchTotalVotes,
+    backendUrl,
+    mapUUID,
+    editMap,
+  });
 
   return (
-    <>
-      <div
-        id="cy"
-        className={classNames(
-          editMap && editType === "cursor" && "cursor-default",
-          editMap && editType === "addNode" && "cursor-copy",
-          editMap && editType === "addEdges" && "cursor-crosshair",
-          editMap && editType === "delete" && "cursor-pointer",
-          !editMap && hoverNode && "cursor-pointer",
-          "absolute z-0 bg-gray-900 w-full h-[calc(100%-60px)]"
-        )}
-        ref={cytoscapeRef}
-      />
-      <ConceptTippy
-        visible={conceptTippyShown}
-        node={nodeSelected}
-        backendUrl={backendUrl}
-        userId={userId}
-        userEmail={userEmail}
-        mapUUID={mapUUID}
-        sessionId={sessionId}
-        hideTippy={hideConceptTippy}
-        reference={cytoscapeRef}
-        learnedNodes={learned}
-        goalNodes={goals}
-        onLearnedClick={onLearnedClick}
-        onSetGoalClick={onSetGoalClick}
-        allowSuggestions={allowSuggestions}
-        buttonPressFunction={buttonPressFunction}
-        userVotes={userVotes}
-        onVote={onVote}
-      />
-    </>
+    <div className="flex flex-column sm:flex-row w-full h-excl-toolbar">
+      <div className={classNames(nodeSelected !== undefined && "w-0 sm:w-[calc(100vw-30rem)] lg:w-[calc(100vw-38rem)]", "relative h-excl-toolbar w-full")}>
+        <div
+            id="cy"
+            className={classNames(
+                editMap && editType === "cursor" && "cursor-default",
+                editMap && editType === "addNode" && "cursor-copy",
+                editMap && editType === "addEdges" && "cursor-crosshair",
+                editMap && editType === "delete" && "cursor-pointer",
+                !editMap && hoverNode && "cursor-pointer",
+                "z-0 w-full bg-gray-900 h-excl-toolbar",
+                nodeSelected !== undefined && "w-0 sm:w-[calc(100vw-30rem)] lg:w-[calc(100vw-38rem)]"
+            )}
+            ref={cytoscapeRef}
+        />
+        <div
+            className={classNames(
+                !editMap && data && nodeSelected && "hidden sm:flex",
+                "flex flex-col md:flex-row items-end gap-4 absolute bottom-0 right-0 m-4"
+            )}
+        >
+          {!editMap && (
+              <GetNextConceptButton
+                  nextConcept={nextConcept}
+                  buttonPressFunction={buttonPressFunction}
+              />
+          )}
+          <ResetPanButton buttonPressFunction={buttonPressFunction} />
+          {!editMap && (
+              <ResetProgressIconButton
+                  buttonPressFunction={buttonPressFunction}
+                  backendUrl={backendUrl}
+                  userId={userId}
+                  mapUUID={mapUUID}
+                  sessionId={sessionId}
+                  setGoalsState={setGoalsState}
+                  setLearnedState={setLearnedState}
+              />
+          )}
+        </div>
+      </div>
+      {/*RIGHT SIDE PANEL*/}
+      {!editMap && data && (
+        <ConceptInfo
+          visible={nodeSelected !== undefined}
+          node={nodeSelected}
+          backendUrl={backendUrl}
+          userId={userId}
+          userEmail={userEmail}
+          mapUUID={mapUUID}
+          sessionId={sessionId}
+          hideConceptInfo={hideConceptPanel}
+          learnedNodes={learned}
+          goalNodes={goals}
+          onLearnedClick={onLearnedClick}
+          onSetGoalClick={onSetGoalClick}
+          allowSuggestions={allowSuggestions}
+          buttonPressFunction={buttonPressFunction}
+          userVotes={userVotes}
+          onVote={onVote}
+          allVotes={data}
+        />
+      )}
+    </div>
   );
 }
