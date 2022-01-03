51b3c1aab1c3b2a59aee830b586afbcb74964622
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-03 11:27:30 +0000

    Make conceptInfo show up in an Overlay on the RHS

--- components/conceptInfo.tsx
+++ components/conceptInfo.tsx
@@ -1,91 +1,26 @@
-import Tippy from "@tippyjs/react";
 import React from "react";
 import { useAsync } from "react-async";
-import { getValidURLs, logContentClick } from "../lib/utils";
+import {
+  getValidURLs,
+  handleFetchResponses,
+  logContentClick,
+} from "../lib/utils";
 import { CheckCircleIcon, FlagIcon } from "@heroicons/react/outline";
 import { IconToggleButtonWithCheckbox, MakeSuggestionButton } from "./buttons";
-import { handleFetchResponses } from "../lib/utils";
-import { classNames } from "../lib/reactUtils";
-import { cacheHeaders } from "../lib/headers";
-import { XIcon } from "@heroicons/react/outline";
+import {appendToArray, classNames} from "../lib/reactUtils";
 import { LoadingSpinner } from "./animations";
+import { NodeSingular } from "cytoscape";
+import { cacheHeaders } from "../lib/headers";
+import Overlay from "./overlay";
 
-export function ConceptTippy({
+export function ConceptInfo({
   visible,
-  node,
-  backendUrl,
-  userId,
-  userEmail,
-  mapUUID,
-  sessionId,
-  hideTippy,
-  reference,
-  learnedNodes,
-  goalNodes,
-  onLearnedClick,
-  onSetGoalClick,
-  allowSuggestions,
-  buttonPressFunction,
-  userVotes,
-  onVote,
-}) {
-  const { data } = useAsync({
-    promiseFn: fetchTotalVotes,
-    backendUrl,
-    mapUUID,
-  });
-  return (
-    <Tippy
-      placement="auto"
-      allowHTML={true}
-      arrow={true}
-      maxWidth={"auto"}
-      visible={visible}
-      duration={[250, 0]}
-      theme={"light"}
-      interactive={true}
-      reference={reference}
-      getReferenceClientRect={
-        node ? node.popperRef().getBoundingClientRect : () => {}
-      }
-      onClickOutside={
-        node ? () => hideTippy(node, userId, sessionId) : () => {}
-      }
-      content={
-        node ? (
-          <ConceptInfo
-            node={node}
-            backendUrl={backendUrl}
-            userId={userId}
-            userEmail={userEmail}
-            mapUUID={mapUUID}
-            sessionId={sessionId}
-            hideTippy={hideTippy}
-            learnedNodes={learnedNodes}
-            goalNodes={goalNodes}
-            onLearnedClick={onLearnedClick}
-            onSetGoalClick={onSetGoalClick}
-            allowSuggestions={allowSuggestions}
-            buttonPressFunction={buttonPressFunction}
-            userVotes={userVotes}
-            onVote={onVote}
-            allVotes={data}
-          />
-        ) : (
-          ""
-        )
-      }
-    />
-  );
-}
-
-function ConceptInfo({
+  hideConceptInfo,
   node,
   backendUrl,
   userId,
   mapUUID,
   sessionId,
-  hideTippy,
   learnedNodes,
   goalNodes,
   onLearnedClick,
@@ -96,97 +31,98 @@ function ConceptInfo({
   onVote,
   userVotes,
   allVotes,
+}: {
+  visible: boolean;
+  node: NodeSingular | undefined;
+  backendUrl: string;
+  userId: string;
+  userEmail: string;
+  mapUUID: string;
+  sessionId: string;
+  hideConceptInfo: () => void;
+  learnedNodes: object;
+  goalNodes: object;
+  onLearnedClick: Function;
+  onSetGoalClick: Function;
+  allowSuggestions: boolean;
+  buttonPressFunction: Function;
+  userVotes: object;
+  onVote: Function;
+  allVotes: object;
 }) {
-  let urls;
-  if (node.data().urls !== undefined) urls = getValidURLs(node.data().urls);
-  else urls = [];
-
-  let linkPreviews = urls.map((url) => (
-    <ConceptLinkPreview
-      key={url}
-      node={node}
-      url={url}
-      userId={userId}
-      backendUrl={backendUrl}
-      mapUUID={mapUUID}
-      sessionId={sessionId}
-      userVotes={userVotes}
-      allVotes={allVotes}
-      onVote={onVote}
-    />
-  ));
-
-  let linkPreviewOrder = urls.map((url, index) => index);
-
-  if (allVotes) {
-    linkPreviewOrder.sort((a, b) => {
-      let bNumVotes;
-      if (allVotes[urls[b]] === undefined) bNumVotes = 0;
-      else bNumVotes = allVotes[urls[b]];
-      let aNumVotes;
-      if (allVotes[urls[a]] === undefined) aNumVotes = 0;
-      else aNumVotes = allVotes[urls[a]];
-      return bNumVotes - aNumVotes;
-    });
-  }
-
+  if (node === undefined) return <></>;
   return (
-    <div className={`text-center bg-white disableTouchActions`}>
-      <h4 className="text-black text-4xl m-0 mb-1 text-center">
-        {node.data().name}
-      </h4>
-      <div className="text-left text-black max-w-screen-sm mt-0 mx-auto mb-4">
-        {node.data().description}
-      </div>
-      <div
-        className="absolute cursor-pointer m-1 sm:m-2 top-0 right-0"
-        onClick={
-          node
-            ? buttonPressFunction(hideTippy, "Top Right Close Concept X")
-            : () => {}
+    <Overlay
+      open={visible}
+      hide={
+        node
+          ? buttonPressFunction(hideConceptInfo, "Top Right Close Concept X")
+          : () => {}
+      }
+    >
+      <div className="flex flex-col text-center h-excl-toolbar w-full overflow-hidden">
+        <h4 className="text-gray-900 text-2xl font-bold sm:text-4xl mb-2 px-4 text-center">
+          {node && node.data().name}
+        </h4>
+        <div className="text-left text-black mt-0 mx-auto mb-4 px-4 max-h-1/5">
+          {node && node.data().description}
+        </div>
+        {node &&
+            <div
+                className="absolute bottom-0 left-0 sm:relative w-full left-0 bottom-0 z-20 bg-white justify-around border-t border-solid border-gray-300 px-2 py-4 grid items-center grid-flow-col">
+              <IconToggleButtonWithCheckbox
+                  checked={!!learnedNodes[node.id()]}
+                  onCheck={() => onLearnedClick(node, userId, sessionId)}
+                  Icon={CheckCircleIcon}
+                  text={"I Know This!"}
+                  colour={"green"}
+              />
+              <IconToggleButtonWithCheckbox
+                  checked={!!goalNodes[node.id()]}
+                  onCheck={() => onSetGoalClick(node, userId, sessionId)}
+                  Icon={FlagIcon}
+                  text={"Set Goal"}
+                  colour={"blue"}
+              />
+            </div>
         }
-      >
-        <span className="sr-only">Close</span>
-        <XIcon className="text-gray-400 w-6 h-6 hover:text-gray-500" />
-      </div>
-      <div className="justify-around border-t border-solid border-gray-600 px-2 py-4 grid items-center grid-flow-col">
-        <div>
-          <IconToggleButtonWithCheckbox
-            checked={!!learnedNodes[node.id()]}
-            onCheck={() => onLearnedClick(node, userId, sessionId)}
-            Icon={CheckCircleIcon}
-            text={"I Know This!"}
-            colour={"green"}
-          />
-        </div>
-        <div>
-          <IconToggleButtonWithCheckbox
-            checked={!!goalNodes[node.id()]}
-            onCheck={() => onSetGoalClick(node, userId, sessionId)}
-            Icon={FlagIcon}
-            text={"Set Goal"}
-            colour={"blue"}
-          />
-        </div>
-      </div>
-      <ol className="list-none pl-0 m-0">
-        {linkPreviewOrder.map((index) => linkPreviews[index])}
-      </ol>
-      {allowSuggestions && (
-        <div className="">
-          {allowSuggestions && (
+        <ol className="shrink grow list-none pl-0 m-0 overflow-auto mb-20 md:mb-0 sm:px-2 w-full">
+          {node &&
+            appendToArray(getAndSortLinkPreviewURLs(node, allVotes).map((url) => (
+              <ConceptLinkPreview
+                key={url}
+                node={node}
+                url={url}
+                userId={userId}
+                backendUrl={backendUrl}
+                mapUUID={mapUUID}
+                sessionId={sessionId}
+                userVotes={userVotes}
+                allVotes={allVotes}
+                onVote={onVote}
+              />
+            )), allowSuggestions && (
+          <div className="pb-2 sm:pb-20">
             <MakeSuggestionButton
               buttonPressFunction={buttonPressFunction}
               userEmail={userEmail}
               buttonName={"Suggest Content"}
-              text={"Suggest Content!"}
+              text={"Suggest Content"}
             />
-          )}
-        </div>
-      )}
-    </div>
+          </div>
+        ))}
+        </ol>
+      </div>
+    </Overlay>
   );
 }
+
+type LinkPreviewData = {
+  status: number;
+  title: string;
+  description: string;
+  image_url: string;
+};
 
 function ConceptLinkPreview({
   node,
@@ -198,6 +134,16 @@ function ConceptLinkPreview({
   userVotes,
   allVotes,
   onVote,
+}: {
+  node: NodeSingular;
+  url: string;
+  backendUrl: string;
+  userId: string;
+  mapUUID: string;
+  sessionId: string;
+  userVotes: object;
+  allVotes: object;
+  onVote: Function;
 }) {
   const { data } = useAsync({
     promiseFn: fetchLinkPreview,
@@ -208,17 +154,16 @@ function ConceptLinkPreview({
   });
 
   return (
-    <li className="text-black relative">
+    <li className="text-gray-900 relative z-10">
       <a
         href={url}
-        style={{ paddingRight: "60px", height: "100px" }}
-        className="bg-gray-100 hover:bg-white visited:bg-gray-300 rounded-sm w-screen-sm overflow-hidden flex text-left m-0.5"
+        className="hover:bg-gray-100 hover:shadow-lg h-24 rounded-sm overflow-hidden flex text-left m-0.5"
         target="_blank"
         rel="noreferrer"
         onClick={() =>
           logContentClick(
             url,
-            node.id(),
+            Number(node.id()),
             backendUrl,
             userId,
             mapUUID,
@@ -226,38 +171,37 @@ function ConceptLinkPreview({
           )
         }
       >
-        <div className="flex items-center max-h-full w-1/4">
+        <div className="flex items-center max-h-full w-20 sm:w-32">
           {data ? (
             <img
-              src={data.image_url}
+              src={(data as LinkPreviewData).image_url}
               className="m-auto max-h-full"
-              alt={data.title}
+              alt={(data as LinkPreviewData).title}
             />
           ) : (
             <LoadingSpinner classes="h-3/5 w-3/5 m-auto" />
           )}
         </div>
-        <div className="max-h-full w-full my-0 ml-1 no-underline overflow-hidden">
-          <h4 className="py-2 overflow-hidden whitespace-nowrap overflow-ellipsis text-xl">
-            {data ? data.title : "Loading..."}
+        <div className="w-[calc(100%-8.25rem)] w-[calc(100%-10.75rem)] mr-0 ml-1 no-underline overflow-hidden">
+          <h4 className="text-lg sm:text-xl sm:py-1 overflow-hidden whitespace-nowrap overflow-ellipsis">
+            {data ? (data as LinkPreviewData).title : "Loading..."}
           </h4>
-          <p className="mt-0 mb-1 font-lg sm:font-base overflow-hidden overflow-ellipsis text-black max-h-70 sm:max-h-40">
-            {data ? data.description : ""}
+          <p className={classNames(data && (data as LinkPreviewData).description ? "h-15" : "h-10", "-mt-0.5 mb-0.5 text-sm overflow-hidden overflow-ellipsis text-gray-800 sm:max-h-40")}>
+            {data ? (data as LinkPreviewData).description : ""}
           </p>
-          <p className="hidden sm:block font-sm text-gray-500 whitespace-nowrap overflow-ellipsis overflow-hidden max-w-xs my-0">
+          <p className={classNames(data && (data as LinkPreviewData).description && "hidden sm:block", "text-xxs sm:text-sm text-gray-500 whitespace-nowrap overflow-ellipsis overflow-hidden max-w-xs my-0")}>
             {url}
           </p>
         </div>
       </a>
-      <div className="absolute right-0 top-0 w-8 h-24 m-3">
-        <div className="vote-arrow upvote">
-          {/*<div className={classNames(allVotes[urlInfo.url] ? "" : "", "triangle up cursor-pointer")} onClick={voteFunction(true, )}/>*/}
+      <div className="absolute right-1 top-0 w-8">
+        <div className="h-8 w-8">
           <div
             className={classNames(
               userVotes[url]
                 ? "border-b-green-500 hover:border-b-green-400"
                 : "border-b-gray-500 hover:border-b-gray-400",
-              "triangle up cursor-pointer border-t-transparent border-l-transparent border-r-transparent"
+              "w-0 h-0 border-[1rem] border-t-0 border-t-transparent border-b-[2rem] cursor-pointer border-t-transparent border-l-transparent border-r-transparent"
             )}
             onClick={
               userVotes[url]
@@ -269,7 +213,7 @@ function ConceptLinkPreview({
           />
         </div>
         {/*Vote count number*/}
-        <div className={"text-sm font-semibold py-1"}>
+        <div className={"text-sm font-semibold py-1.5"}>
           {allVotes && Math.abs(allVotes[url]) > 5
             ? userVotes[url] === true
               ? allVotes[url] + 1
@@ -278,14 +222,13 @@ function ConceptLinkPreview({
               : allVotes[url]
             : "Vote"}
         </div>
-        <div className="vote-arrow downvote">
-          {/*<div className={classNames(allVotes[urlInfo.url] === false ? "" : "", "triangle down cursor-pointer")} onClick={voteFunction(false, )}/>*/}
+        <div className="h-8 w-8">
           <div
             className={classNames(
               userVotes[url] === false
                 ? "border-t-red-500 hover:border-t-red-400"
                 : "border-t-gray-500 hover:border-t-gray-400",
-              "triangle down cursor-pointer border-b-transparent border-l-transparent border-r-transparent"
+              "w-0 h-0 border-[1rem] border-b-0 border-b-transparent border-t-[2rem] cursor-pointer border-b-transparent border-l-transparent border-r-transparent"
             )}
             onClick={
               userVotes[url] === false
@@ -301,7 +244,40 @@ function ConceptLinkPreview({
   );
 }
 
-const fetchLinkPreview = async ({ node, url, backendUrl, mapUUID }) => {
+function getAndSortLinkPreviewURLs(
+  node: NodeSingular,
+  allVotes: object
+): Array<string> {
+  let urls;
+  if (node !== undefined && node.data().urls !== undefined)
+    urls = getValidURLs(node.data().urls);
+  else urls = [];
+
+  if (allVotes) {
+    urls.sort((urlA, urlB) => {
+      let bNumVotes;
+      if (allVotes[urlB] === undefined) bNumVotes = 0;
+      else bNumVotes = allVotes[urlB];
+      let aNumVotes;
+      if (allVotes[urlA] === undefined) aNumVotes = 0;
+      else aNumVotes = allVotes[urlA];
+      return bNumVotes - aNumVotes;
+    });
+  }
+  return urls;
+}
+
+const fetchLinkPreview = async ({
+  node,
+  url,
+  backendUrl,
+  mapUUID,
+}: {
+  node: NodeSingular;
+  url: string;
+  backendUrl: string;
+  mapUUID: string;
+}): Promise<object> => {
   const response = await fetch(
     `${backendUrl}/api/v0/link_previews?` +
       new URLSearchParams({
@@ -315,24 +291,14 @@ const fetchLinkPreview = async ({ node, url, backendUrl, mapUUID }) => {
       headers: cacheHeaders,
     }
   );
-  let responseJson = await handleFetchResponses(response, backendUrl);
+  let responseJson: LinkPreviewData = (await handleFetchResponses(
+    response,
+    backendUrl
+  )) as LinkPreviewData;
   if (response.status !== 200 && responseJson.status !== 201)
-    responseJson = { title: "", description: "", image_url: "" };
+    responseJson = { title: "", description: "", image_url: "", status: 400 };
   if (responseJson.title === "") responseJson.title = node.data().name;
   if (responseJson.image_url === "")
     responseJson.image_url = "/images/learney_logo_256x256.png";
   return responseJson;
-};
-
-const fetchTotalVotes = async ({ backendUrl, mapUUID }) => {
-  const response = await fetch(
-    `${backendUrl}/api/v0/total_vote_count?` +
-      new URLSearchParams({ map: mapUUID }),
-    {
-      method: "GET",
-      headers: cacheHeaders,
-    }
-  );
-  if (!response.ok) throw new Error(response.status.toString());
-  return handleFetchResponses(response, backendUrl);
 };
