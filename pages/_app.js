9bff11c86d2cadce945fbfa446da4b828abb654e
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-03 17:43:34 +0000

    Fix build issue

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import "../styles/global.css";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/scale.css";
 import "tippy.js/themes/light.css";
-import "react-select2-wrapper/css/select2.css";
 import { ErrorBoundary } from "../ErrorBoundary";
 
 export default function App({ Component, pageProps }) {
