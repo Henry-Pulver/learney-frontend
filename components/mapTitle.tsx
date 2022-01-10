786bd9b4890ed05a1ae6804e77b6d059aba128cc
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-10 18:19:38 +0000

    Track user clicks on title box and description box
    Mostly to see if expansion and minimisation is intuitive.

--- components/mapTitle.tsx
+++ components/mapTitle.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
-import { classNames } from "../lib/reactUtils";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
+import { classNames } from "../lib/reactUtils";
+import { ButtonPressFunction } from "../lib/types";
 
 export default function MapTitle({
   title,
   description,
+  buttonPressFunction,
 }: {
   title: string;
   description: string;
+  buttonPressFunction: ButtonPressFunction;
 }) {
   const [descriptionExpanded, setDescriptionExpanded] =
     useState<boolean>(false);
   if (title.length === 0) return <></>;
-  console.log(description);
   return (
     <div
       className="absolute z-10 left-8 top-6 bg-white rounded-lg max-w-xl px-4 py-2"
       onClick={
-        !descriptionExpanded ? () => setDescriptionExpanded(true) : () => {}
+        !descriptionExpanded
+          ? buttonPressFunction(
+              () => setDescriptionExpanded(true),
+              "Map Title Anywhere (Expand)"
+            )
+          : buttonPressFunction(() => {}, "Map Title Anywhere (Invalid)")
       }
     >
       <div className="relative">
@@ -36,8 +43,14 @@ export default function MapTitle({
         {description.length > 64 && (
           <div className="absolute right-0 bottom-0">
             <button
-              className="gray-icon-btn-no-padding rounded-xl text-gray-500 hover:text-gray-600"
-              onClick={() => setDescriptionExpanded((expanded) => !expanded)}
+              className="gray-icon-btn-no-padding rounded-xl text-gray-500 hover:text-gray-600 z-20"
+              onClick={buttonPressFunction(
+                (e) => {
+                    e.stopPropagation(); // Stops parent div's onClick function from being called!
+                    setDescriptionExpanded((expanded) => !expanded);
+                  },
+                "Map Title Expand/Minimise Button"
+              )}
             >
               <span className="sr-only">
                 {descriptionExpanded
