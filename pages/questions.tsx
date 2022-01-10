90a4147e2d499f47812744fd05519f7afe55a8d8
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-10 13:24:43 +0000

    Add /questions endpoint

---
+++ pages/questions.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import MapPage from "../components/mapPage";
+import { cacheHeaders } from "../lib/headers";
+
+export default function OriginalMap({
+  mapJson,
+  mapUUID,
+  backendUrl,
+}: {
+  mapJson: string;
+  mapUUID: string;
+  backendUrl: string;
+}) {
+  return (
+    <MapPage
+      backendUrl={backendUrl}
+      mapUUID={mapUUID}
+      mapUrlExtension={"original_map"}
+      allowSuggestions={true}
+      editMap={false}
+      mapJsonString={mapJson}
+      questionsEnabled={true}
+    />
+  );
+}
+
+export async function getServerSideProps() {
+  const mapResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v0/knowledge_maps?` +
+      new URLSearchParams({ url_extension: "original_map" }),
+    {
+      method: "GET",
+      headers: cacheHeaders,
+    }
+  );
+  const mapInfoJson = await mapResponse.json();
+  return {
+    props: {
+      mapJson: mapInfoJson.map_json,
+      mapUUID: mapInfoJson.unique_id,
+      backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
+    },
+  };
+}
