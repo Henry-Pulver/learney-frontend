44ef247bd1d03b67a2448742874f235c98b70c00
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-13 13:03:14 +0000

    Bug fixes with questions page

--- pages/questions.tsx
+++ pages/questions.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MapPage from "../components/mapPage";
 import { cacheHeaders } from "../lib/headers";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default function OriginalMap({
   mapTitle,
@@ -21,7 +22,7 @@ export default function OriginalMap({
       mapDescription={mapDescription}
       backendUrl={backendUrl}
       mapUUID={mapUUID}
-      mapUrlExtension={"original_map"}
+      mapUrlExtension={"questionsmap"}
       allowSuggestions={true}
       editMap={false}
       mapJsonString={mapJson}
@@ -30,23 +31,39 @@ export default function OriginalMap({
   );
 }
 
-export async function getServerSideProps() {
-  const mapResponse = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v0/knowledge_maps?` +
-      new URLSearchParams({ url_extension: "original_map" }),
-    {
-      method: "GET",
-      headers: cacheHeaders,
+// @ts-ignore
+export const getServerSideProps = withPageAuthRequired({
+  returnTo: "",
+  async getServerSideProps(ctx) {
+    const mapResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v0/knowledge_maps?` +
+        new URLSearchParams({ url_extension: "questionsmap" }),
+      {
+        method: "GET",
+        headers: cacheHeaders,
+      }
+    );
+    const session = getSession(ctx.req, ctx.res);
+    const mapInfoJson = await mapResponse.json();
+    // TODO: Currently we redirect, change to showing custom 'blocked' page & link to map
+    if (
+      ![
+        // ...mapInfoJson.usersWithAccess.map((user) => user.toLowerCase()),
+        "henrypulver13@gmail.com", // Henry can access every map
+      ].includes(session.user.email.toLowerCase())
+    ) {
+      return {
+        redirect: { destination: `/` },
+      };
     }
-  );
-  const mapInfoJson = await mapResponse.json();
-  return {
-    props: {
-      mapTitle: mapInfoJson.title,
-      mapDescription: mapInfoJson.description,
-      mapJson: mapInfoJson.map_json,
-      mapUUID: mapInfoJson.unique_id,
-      backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
-    },
-  };
-}
+    return {
+      props: {
+        mapTitle: mapInfoJson.title,
+        mapDescription: mapInfoJson.description,
+        mapJson: mapInfoJson.map_json,
+        mapUUID: mapInfoJson.unique_id,
+        backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
+      },
+    };
+  },
+});
