3570d4d78c34f397090e3e6f0344e6c68a5a3da5
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-20 13:32:54 +0000

    Change buttonPressFunction to state
    This fixes several bugs - previously it required userId from the start
    and wouldn't necessarily update correctly when userId was updated. Now
    this is solved plus all buttons are deactivated until userId is determined
    and button presses can be tracked

--- lib/types.ts
+++ lib/types.ts
@@ -1,8 +1,7 @@
 import { UserProfile } from "@auth0/nextjs-auth0";
 
 type ButtonPressCallback = (...args) => void;
-export type ButtonPressFunction = (
-  callback: ButtonPressCallback,
-  name: string
-) => ButtonPressCallback;
+export type ButtonPressFunction =
+  | ((callback: ButtonPressCallback, name: string) => ButtonPressCallback)
+  | (() => () => void);
 export type UserState = UserProfile | undefined;
