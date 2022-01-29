6a045df8f850a97c4faa9d400ebb4eb4c707665c
Henry Pulver <henrypulver@hotmail.co.uk>
2022-01-29 18:22:56 +0000

    Make sure the notification shows up over the concept overlay x button

--- components/notifications.tsx
+++ components/notifications.tsx
@@ -20,7 +20,7 @@ export function Notification({
       {/* Global notification live region, render this permanently at the end of the document */}
       <div
         aria-live="assertive"
-        className="fixed inset-0 flex items-end px-4 py-6 sm:py-20 sm:px-6 pointer-events-none sm:items-start"
+        className="fixed inset-0 z-20 flex items-end px-4 py-6 sm:py-20 sm:px-6 pointer-events-none sm:items-start"
       >
         <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
           {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
