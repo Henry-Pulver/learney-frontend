d53529214fddd72f6f42cbeb81b21494742132be
Henry Pulver <henrypulver@hotmail.co.uk>
2022-02-01 12:22:50 +0000

    Add progress modal when you click on levels

--- components/questions/progressBars.tsx
+++ components/questions/progressBars.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { classNames } from "../../lib/reactUtils";
+import { BadgeCheckIcon } from "@heroicons/react/outline";
+import LevelBadge from "./levelBadge";
 
-export function LevelsProgressBar(props: { knowledgeLevel: number }) {
+export function LevelsProgressBar(props: {
+  knowledgeLevel: number;
+  maxKnowledgeLevel: number;
+  onClick?: () => void;
+}) {
   return (
-    <div className="max-w-lg w-full flex justify-between content-center text-gray-900 font-bold">
-      <p
-        className={classNames(
-          "text-base bg-green-100 p-auto w-7 h-7 rounded-full border-solid border-green-600 border-2",
-          (!props.knowledgeLevel || Math.floor(props.knowledgeLevel)) === 0 &&
-            "invisible"
-        )}
-      >
-        {Math.floor(props.knowledgeLevel)}
-      </p>
-      <div className="w-5/6 my-2">
+    <div className="max-w-xl w-full grid grid-cols-8 text-sm items-center text-gray-900">
+      <div className="col-start-2 col-span-6">
         <ProgressBar
           colour="green"
+          className="cursor-pointer"
+          onClick={props.onClick}
           percentFilled={
-            props.knowledgeLevel
+            props.knowledgeLevel < props.maxKnowledgeLevel
               ? Math.max(
                   Math.min(
                     realPercentageToProgress(props.knowledgeLevel % 1),
@@ -25,23 +24,38 @@ export function LevelsProgressBar(props: { knowledgeLevel: number }) {
                   ),
                   0
                 )
-              : null
+              : 100
           }
         />
       </div>
-      <p className="text-base bg-green-100 p-auto w-7 h-7 rounded-full border-solid border-green-600 border-2">
-        {props.knowledgeLevel ? Math.floor(props.knowledgeLevel) + 1 : 1}
-      </p>
+      <LevelBadge
+        overallClassName={classNames(
+          props.knowledgeLevel >= props.maxKnowledgeLevel && "invisible",
+          "visible col-span-1 justify-self-center cursor-pointer text-sm"
+        )}
+        knowledgeLevel={Math.floor(Math.max(props.knowledgeLevel, 0) + 1)}
+        achieved={true}
+        onClick={props.onClick}
+      />
     </div>
   );
 }
 
 export function ProgressBar(props: {
   colour?: "green" | "blue" | "orange" | "red";
-  percentFilled: number | null;
+  percentFilled: number | null; // Allow null to stop animating progress
+  // bar down when going from a concept with progress to one without
+  onClick?: () => void;
+  className?: string;
 }) {
   return (
-    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+    <div
+      className={classNames(
+        props.className,
+        "w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700"
+      )}
+      onClick={props.onClick ? props.onClick : () => {}}
+    >
       <div
         className={classNames(
           (props.colour === undefined || props.colour === "green") &&
@@ -49,7 +63,7 @@ export function ProgressBar(props: {
           props.colour === "blue" && "bg-blue-600",
           props.colour === "orange" && "bg-orange-600",
           props.colour === "red" && "bg-red-600",
-          props.percentFilled ? "duration-1000" : "duration-0",
+          props.percentFilled !== null ? "duration-1000" : "duration-0",
           `h-2.5 rounded-full transition-all ease-in-out`
         )}
         style={
