356532ff2df07ce69489a6feea83c602edf7a4f1
Henry Pulver <henrypulver@hotmail.co.uk>
2022-04-13 09:55:14 +0100

    Remove old card component

--- components/conceptInfo.tsx
+++ components/conceptInfo.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useAsync } from "react-async";
+import React from "react";
 import { CheckCircleIcon, FlagIcon } from "@heroicons/react/outline";
 import { IconToggleButtonWithCheckbox, MakeSuggestionButton } from "./buttons";
 import { appendToArray, classNames } from "../lib/reactUtils";
@@ -11,10 +10,8 @@ import {
   getValidURLs,
   handleFetchResponses,
   isAnonymousUser,
-  logContentClick,
   setURLQuery,
 } from "../lib/utils";
-import { LoadingSpinner } from "./animations";
 import { cacheHeaders, jsonHeaders } from "../lib/headers";
 import LevelBadge from "./questions/levelBadge";
 import { LevelsProgressBar } from "./questions/progressBars";
@@ -22,7 +19,7 @@ import { useRouter } from "next/router";
 import { signInTooltip } from "../lib/learningAndPlanning/learningAndPlanning";
 import parse from "html-react-parser";
 import { ContentModal } from "./contentModal";
-import { ContentCard } from "./contentCard";
+import { ContentCard, LinkPreviewData } from "./contentCard";
 
 type OnVote = (node: NodeSingular, url: string, up: boolean | null) => void;
 
@@ -190,7 +187,7 @@ export function ConceptInfo({
             {node &&
               appendToArray(
                 getAndSortLinkPreviewURLs(node, allVotes).map((url, idx) => (
-                  <ConceptLinkPreview
+                  <ContentCard
                     key={`${url}-${idx}`}
                     node={node}
                     url={url}
@@ -214,7 +211,7 @@ export function ConceptInfo({
                     No suggested content for {node.data().name}.
                   </div>
                 ),
-                allowSuggestions && (
+                allowSuggestions ? (
                   // <div> necessary as otherwise the button grows to the width of the list element
                   <div key="Make suggestion button">
                     <MakeSuggestionButton
@@ -224,217 +221,13 @@ export function ConceptInfo({
                       text={"Suggest Content"}
                     />
                   </div>
-                )
+                ) : (
+                  <div className="h-24" />
+                ) // empty div to prevent description overflowing off screen
               )}
           </ol>
         </div>
       </Overlay>
-    </>
-  );
-}
-
-type LinkPreviewData = {
-  status: number;
-  title: string;
-  description: string;
-  image_url: string;
-  checked: boolean;
-};
-
-function ConceptLinkPreview({
-  node,
-  url,
-  backendUrl,
-  userId,
-  mapUUID,
-  sessionId,
-  userVotes,
-  allVotes,
-  onVote,
-  setIsContentModalOpen,
-  setContentURL,
-  setContentType,
-}: {
-  node: NodeSingular;
-  url: string;
-  backendUrl: string;
-  userId: string;
-  mapUUID: string;
-  sessionId: string;
-  userVotes: object;
-  allVotes: object;
-  onVote: OnVote;
-  setIsContentModalOpen: (open: boolean) => void;
-  setContentURL: (url: string) => void;
-  setContentType: (type: string) => void;
-}) {
-  const { data, isLoading } = useAsync({
-    promiseFn: fetchLinkPreview,
-    node,
-    url,
-    backendUrl,
-    mapUUID,
-    userId,
-  });
-  const [checked, setChecked] = useState(false);
-  useEffect(() => {
-    if (!isLoading) setChecked(data.checked);
-  }, [data, isLoading]);
-  const conceptOnClickHandler = async (externalURL: string) => {
-    let extractedURL = "";
-    let contentType = "";
-    try {
-      const res = await fetch("/api/iframe/url", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          contentURL: externalURL,
-        }),
-      });
-      const resJSON = await res.json();
-      extractedURL = resJSON.url;
-      contentType = resJSON.type;
-    } catch (err) {
-      console.warn("Error while handling iframe URL:", err);
-    }
-    if (extractedURL.length > 0) {
-      setContentURL(extractedURL);
-      setContentType(contentType);
-      setIsContentModalOpen(true);
-    } else {
-      window.open(url, "_blank");
-    }
-  };
-  return (
-    <>
-      <li className="py-auto relative flex cursor-pointer flex-row text-gray-900">
-        <div
-          className={classNames(
-            "mx-0.5 my-1 flex h-24 w-full overflow-hidden rounded bg-white text-left hover:bg-gray-100 hover:shadow-md ",
-            checked && "bg-green-100 hover:bg-green-200 "
-          )}
-          onClick={async () => {
-            await conceptOnClickHandler(url);
-            logContentClick(
-              url,
-              Number(node.id()),
-              backendUrl,
-              userId,
-              mapUUID,
-              sessionId
-            );
-          }}
-        >
-          <div className="relative flex h-full flex-col justify-center pr-4">
-            <input
-              type="checkbox"
-              checked={checked}
-              onClick={(e) => e.stopPropagation()}
-              onChange={() => {
-                setChecked(!checked);
-                postChecked(node, url, backendUrl, mapUUID, userId);
-              }}
-              className={classNames(
-                checked &&
-                  "ring-2 !ring-green-500 ring-offset-2 focus:!ring-2 focus:!ring-offset-2",
-                "ml-2 h-6 w-6 cursor-pointer select-none rounded border-gray-300 text-green-600 focus:ring-0 focus:ring-offset-0"
-              )}
-            />
-          </div>
-          <div className="flex max-h-full w-20 items-center sm:w-32">
-            {data ? (
-              <img
-                src={(data as LinkPreviewData).image_url}
-                className="m-auto max-h-full"
-                alt={(data as LinkPreviewData).title}
-              />
-            ) : (
-              <LoadingSpinner classes="h-3/5 w-3/5 m-auto" />
-            )}
-          </div>
-          <div className="mr-0 ml-1 w-[calc(100%-8.25rem)] grow overflow-hidden text-ellipsis no-underline sm:w-[calc(100%-13.25rem)]">
-            <h4 className="overflow-hidden text-ellipsis whitespace-nowrap text-lg sm:py-1 sm:text-xl">
-              {data ? (data as LinkPreviewData).title : "Loading..."}
-            </h4>
-            <p
-              className={classNames(
-                data && (data as LinkPreviewData).description ? "h-15" : "h-10",
-                "-mt-0.5 mb-0.5 overflow-hidden text-ellipsis text-sm text-gray-800 sm:max-h-40"
-              )}
-            >
-              {data ? (data as LinkPreviewData).description : ""}
-            </p>
-            <p
-              className={classNames(
-                data &&
-                  (data as LinkPreviewData).description &&
-                  "hidden sm:block",
-                "text-xxs my-0 max-w-xs overflow-hidden text-ellipsis whitespace-nowrap text-gray-500 sm:text-sm"
-              )}
-            >
-              {url}
-            </p>
-          </div>
-          <div className="w-10" />
-        </div>
-        <div className="absolute right-1 top-0 my-1 w-8">
-          <div className="h-8 w-8">
-            <div
-              className={classNames(
-                userVotes[url]
-                  ? "border-b-green-500 hover:border-b-green-400"
-                  : "border-b-gray-500 hover:border-b-gray-400",
-                "h-0 w-0 cursor-pointer border-[1rem] border-t-0 border-b-[2rem] border-t-transparent border-t-transparent border-l-transparent border-r-transparent"
-              )}
-              onClick={
-                userVotes[url]
-                  ? () => onVote(node, url, null) // true  ->  null
-                  : userVotes[url] === null || userVotes[url] === undefined
-                  ? () => onVote(node, url, true) // null  ->  true
-                  : () => onVote(node, url, null) // false ->  null
-              }
-            />
-          </div>
-          {/*Vote count number*/}
-          <div className={"py-1.5 text-sm font-semibold"}>
-            {allVotes && Math.abs(allVotes[url]) > 5
-              ? userVotes[url] === true
-                ? allVotes[url] + 1
-                : userVotes[url] === false
-                ? allVotes[url] - 1
-                : allVotes[url]
-              : "Vote"}
-          </div>
-          <div className="h-8 w-8">
-            <div
-              className={classNames(
-                userVotes[url] === false
-                  ? "border-t-red-500 hover:border-t-red-400"
-                  : "border-t-gray-500 hover:border-t-gray-400",
-                "h-0 w-0 cursor-pointer border-[1rem] border-b-0 border-t-[2rem] border-b-transparent border-b-transparent border-l-transparent border-r-transparent"
-              )}
-              onClick={
-                userVotes[url] === false
-                  ? () => onVote(node, url, null) // false ->  null
-                  : userVotes[url] === null || userVotes[url] === undefined
-                  ? () => onVote(node, url, false) // null  ->  false
-                  : () => onVote(node, url, null) // true  ->  null
-              }
-            />
-          </div>
-        </div>
-      </li>
-      <ContentCard
-        node={node}
-        url={url}
-        backendUrl={backendUrl}
-        mapUUID={mapUUID}
-        userId={userId}
-        userVotes={userVotes}
-        onVote={onVote}
-      />
     </>
   );
 }
@@ -462,27 +255,22 @@ function getAndSortLinkPreviewURLs(
   return urls;
 }
 
-const fetchLinkPreview = async ({
-  node,
-  url,
-  backendUrl,
-  mapUUID,
-  userId,
-}: {
+export const fetchLinkPreview = async (props: {
   node: NodeSingular;
   url: string;
   backendUrl: string;
   mapUUID: string;
   userId: string;
 }): Promise<LinkPreviewData> => {
+  console.log("fetchLinkPreview", props);
   const response = await fetch(
-    `${backendUrl}/api/v0/link_previews?` +
+    `${props.backendUrl}/api/v0/link_previews?` +
       new URLSearchParams({
-        map: mapUUID,
-        concept: node.data().name,
-        concept_id: node.id(),
-        url: url,
-        user_id: userId,
+        map: props.mapUUID,
+        concept: props.node.data().name,
+        concept_id: props.node.id(),
+        url: props.url,
+        user_id: props.userId,
       }),
     {
       method: "GET",
@@ -491,17 +279,29 @@ const fetchLinkPreview = async ({
   );
   let responseJson: LinkPreviewData = (await handleFetchResponses(
     response,
-    backendUrl
+    props.backendUrl
   )) as LinkPreviewData;
   if (response.status !== 200 && responseJson.status !== 201)
     responseJson = {
+      status: 400,
+      map: props.mapUUID,
+      concept: props.node.data().name,
+      concept_id: props.node.id(),
+      url: props.url,
       title: "",
       description: "",
       image_url: "",
-      status: 400,
+      content_type: "",
+      estimated_time_to_complete: "",
+      author: {
+        id: "",
+        name: "",
+        image_url: "",
+      },
+      tags: [],
       checked: false,
     };
-  if (responseJson.title === "") responseJson.title = node.data().name;
+  if (responseJson.title === "") responseJson.title = props.node.data().name;
   if (responseJson.image_url === "")
     responseJson.image_url = "/images/learney_logo_256x256.png";
   return responseJson;
