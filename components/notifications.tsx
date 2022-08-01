e0312026519a91abcc8e068cb330712929ef550a
Henry Pulver <henrypulver@hotmail.co.uk>
2022-08-01 09:32:05 +0100

    Make cross visible on mobile

--- components/notifications.tsx
+++ components/notifications.tsx
@@ -79,6 +79,7 @@ export function Notification({
                           show: false,
                         }))
                       }
+                      visibleOnMobile={true}
                     />
                   )}
                 </div>
